c8743bb4513885a77b17a8acc1a2d2a4e5ec96f6
faideleon <faideleon25@gmail.com>
2026-09-09 22:42:30 +0800

    Added and Edited Github Contributions Grid

--- script/script.js
+++ script/script.js
@@ -19,6 +19,7 @@ async function getContributions() {
     const myContribution = await fetch('https://github-contributions-api.jogruber.de/v4/Faisal-Khan06');
     const data = await myContribution.json();
 
+    showContributions(data.contributions);
 
     const totalContributions = data.total['2026'];
 
@@ -64,6 +65,63 @@ function getStreak(gitData) {
 
     }
     return maxStreak;
+}
+
+function showContributions(gitData) {
+    const container = document.querySelector('.graph-content');
+
+    gitData.forEach(day => {
+        const element = document.createElement('div');
+        element.classList.add('day');
+
+        if (day.count > 0) {
+            if (day.count >= 4) {
+                element.classList.add('fifth-color');
+            }
+            if (day.count === 3) {
+                element.classList.add('fourth-color');
+            }
+            if (day.count === 2) {
+                element.classList.add('third-color');
+            }
+            if (day.count === 1) {
+                element.classList.add('second-color');
+            }
+        } else {
+            element.classList.add('first-color');
+        }
+
+
+        container.append(element);
+    });
+
+    hoverContributions(gitData);
+}
+
+function hoverContributions(gitData) {
+
+    const containerHover = document.querySelectorAll('.day');
+    const main_container = document.querySelector('.graph');
+
+    for (let i = 0; i < containerHover.length; i++) {
+
+        containerHover[i].addEventListener('mouseenter', () => {
+            containerHover[i].style.position = 'relative';
+            const element = document.createElement('div');
+            element.classList.add('tooltip');
+            element.innerHTML = `<h2>${gitData[i].count} contributions</h2><h5>${gitData[i].date}</h5>`;
+
+            containerHover[i].append(element);
+        });
+
+        containerHover[i].addEventListener('mouseleave', () => {
+            const element = document.querySelector('.tooltip');
+            if (element) {
+                element.remove();
+            }
+
+        });
+    }
 }
 
 getContributions();
